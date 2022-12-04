f13b2e84a26eff2e4dad50f8a1cfaf138f66f57b
Elliot Dewhurst <elliot+github@dewhurst.me>
2022-12-04 09:57:29 +0000

    Added day 4 benchmark and refactor to use Range

--- Day4/Program.cs
+++ Day4/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
 
 namespace Day4
 {
@@ -9,28 +8,10 @@ namespace Day4
         static void Main(string[] args)
         {
             var lines = File.ReadLines("input.txt");
-            var pairs = lines.Select(x => x.Split(","));
-            var fullOverlapCounter = 0;
-            var partialOverlapCounter = 0;
-
-            foreach (var pair in pairs)
-            {
-                var firstRange = pair[0].Split("-").Select(x => int.Parse(x)).ToArray();
-                var secondRange = pair[1].Split("-").Select(x => int.Parse(x)).ToArray();
-
-                if (DoRangesFullyOverlap(firstRange, secondRange)) fullOverlapCounter++;
-                if (DoRangesPartiallyOverlap(firstRange, secondRange)) partialOverlapCounter++;
-            }
-
-            Console.WriteLine(fullOverlapCounter);
-            Console.WriteLine(partialOverlapCounter);
+            var solver = new Solver();
+            
+            Console.WriteLine($"Part 1: {solver.SolvePart1(lines)}");
+            Console.WriteLine($"Part 2: {solver.SolvePart2(lines)}");
         }
-
-        static bool DoRangesPartiallyOverlap(int[] firstRange, int[] secondRange) =>
-            firstRange[0] <= secondRange[1] && secondRange[0] <= firstRange[1];
-
-        static bool DoRangesFullyOverlap(int[] firstRange, int[] secondRange) =>
-            firstRange[0] <= secondRange[0] && firstRange[1] >= secondRange[1] ||
-            secondRange[0] <= firstRange[0] && secondRange[1] >= firstRange[1];
     }
 }
---
+++ Day4/Solver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day4
+{
+    public class Solver
+    {
+        public int SolvePart1(IEnumerable<string> lines)
+        {
+            var pairs = lines.Select(x => x.Split(","));
+            var counter = 0;
+            foreach (var pair in pairs)
+            {
+                var ranges = pair.Select(GetRange).ToArray();
+                if (DoRangesFullyOverlap(ranges[0], ranges[1])) counter++;
+            }
+            return counter;
+        }
+
+        public int SolvePart2(IEnumerable<string> lines)
+        {
+            var pairs = lines.Select(x => x.Split(","));
+            var counter = 0;
+            foreach (var pair in pairs)
+            {
+                var ranges = pair.Select(GetRange).ToArray();
+                if (DoRangesPartiallyOverlap(ranges[0], ranges[1])) counter++;
+            }
+            return counter;
+        }
+
+        static bool DoRangesPartiallyOverlap(Range firstRange, Range secondRange) =>
+            firstRange.Start.Value <= secondRange.End.Value && secondRange.Start.Value <= firstRange.End.Value;
+
+        static bool DoRangesFullyOverlap(Range firstRange, Range secondRange) =>
+            firstRange.Start.Value <= secondRange.Start.Value && firstRange.End.Value >= secondRange.End.Value ||
+            secondRange.Start.Value <= firstRange.Start.Value && secondRange.End.Value >= firstRange.End.Value;
+
+        static Range GetRange(string range) 
+        {
+            var split = range.Split("-").Select(x => int.Parse(x)).ToArray();
+            return new Range(split[0], split[1]);
+        }
+    }
+}
