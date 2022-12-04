db4735e47e684bdd05fc038c04bdf0270ae7fd25
Elliot Dewhurst <elliot+github@dewhurst.me>
2022-12-04 09:25:04 +0000

    Day4

---
+++ Day4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var lines = File.ReadLines("input.txt");
+            var pairs = lines.Select(x => x.Split(","));
+            var fullOverlapCounter = 0;
+            var partialOverlapCounter = 0;
+
+            foreach (var pair in pairs)
+            {
+                var firstRange = pair[0].Split("-").Select(x => int.Parse(x)).ToArray();
+                var secondRange = pair[1].Split("-").Select(x => int.Parse(x)).ToArray();
+
+                if (DoRangesFullyOverlap(firstRange, secondRange)) fullOverlapCounter++;
+                if (DoRangesPartiallyOverlap(firstRange, secondRange)) partialOverlapCounter++;
+            }
+
+            Console.WriteLine(fullOverlapCounter);
+            Console.WriteLine(partialOverlapCounter);
+        }
+
+        static bool DoRangesPartiallyOverlap(int[] firstRange, int[] secondRange)
+        {
+            return firstRange[0] <= secondRange[1] && secondRange[0] <= firstRange[1];
+        }
+
+        static bool DoRangesFullyOverlap(int[] firstRange, int[] secondRange)
+        {
+            if (firstRange[0] <= secondRange[0] && firstRange[1] >= secondRange[1]) return true;
+            if (secondRange[0] <= firstRange[0] && secondRange[1] >= firstRange[1]) return true;
+            return false;
+        }
+    }
+}
