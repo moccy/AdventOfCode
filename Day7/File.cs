6f5f5cbf42cff4ffd96bc10578d04b1988ac50f7
Elliot Dewhurst <elliot+github@dewhurst.me>
2022-12-07 10:15:50 +0000

    Day 7

---
+++ Day7/File.cs
@@ -0,0 +1,8 @@
+﻿namespace Day7
+{
+    class File
+    {
+        public string Name { get; set; }
+        public long Size { get; set; }
+    }
+}
---
+++ Day7/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace Day7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const long totalDiskSpace = 70000000;
+            const long updateSize = 30000000;
+            var lines = System.IO.File.ReadLines("input.txt");
+            var fileSystem = new FileSystem();
+            fileSystem.Process(lines);
+
+            Console.WriteLine(fileSystem.Directories.Where(d => d.Size <= 100000).Sum(d => d.Size));
+
+            var remainingDiskSpace = totalDiskSpace - fileSystem.RootDirectory.Size;
+            var neededSpace = updateSize - remainingDiskSpace;
+            Console.WriteLine(fileSystem.Directories.Where(d => d.Size > neededSpace).OrderBy(x => x.Size).First().Size);
+        }
+    }
+}
