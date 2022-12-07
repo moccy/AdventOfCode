f7bafffe0f52c5780384a631fbd37553b5c4c335
Elliot Dewhurst <elliot+github@dewhurst.me>
2022-12-07 10:15:37 +0000

    Day 6 Benchmark

---
+++ Day6.Tests.Benchmark/Program.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace Day6.Tests.Benchmark
+{
+    public class Benchmark
+    {
+        private readonly string _dataStream;
+
+        public Benchmark()
+        {
+            _dataStream = File.ReadAllText("input.txt");
+        }
+
+        [Benchmark]
+        public void Part1() => Day6.Program.GetStartOfPacketMarker(_dataStream);
+
+        [Benchmark]
+        public void Part2() => Day6.Program.GetStartOfMessageMarker(_dataStream);
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run<Benchmark>();
+        }
+    }
+}
--- Day6/Program.cs
+++ Day6/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 namespace Day6
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
